1a73b4b83c9ab8933992efc95dd97937678d764e
JFaulk1434 <j.faulk@me.com>
2022-09-08 14:32:39 -0400

    changed path

--- gitpull.py
+++ gitpull.py
@@ -4,7 +4,7 @@ from os.path import exists as file_exists
 ACCOUNT_NAME = "Admin"
 GIT_USER = "JFaulk1434"
 GIT_REPO = "https://github.com/JFaulk1434/RaspPi.git"
-FPATH = f'/home/{ACCOUNT_NAME}/Documents/pyControl'
+FPATH = f'/home/{ACCOUNT_NAME}/Documents/RaspPi'
 FNAME = "gitpull.py"
 
 
