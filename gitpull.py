80d1198b466669b6106494fb018ef7729556b488
JFaulk1434 <j.faulk@me.com>
2022-09-08 14:10:10 -0400

    test

--- gitpull.py
+++ gitpull.py
@@ -20,7 +20,7 @@ def clone():
         print(f'pulling from github...')
         print(f'adding {FNAME} to {FPATH}')
     else:
-        subprocess.Popen(['git', 'fetch', GIT_REPO])
+        subprocess.Popen(['git', 'pull --prune', GIT_REPO])
         print(f'Files already detected...')
         print(f'ignoring')
 # def clone():
--- pyControl.py
+++ pyControl.py
@@ -3,7 +3,7 @@ import time
 import logging
 import json
 
-# test test test
+
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename="log.txt", level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger()
