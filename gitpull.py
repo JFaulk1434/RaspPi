72333e031a6a6e04dbac89f2849b3bce27a02ff8
JFaulk1434 <j.faulk@me.com>
2022-09-08 14:15:54 -0400

    2

--- gitpull.py
+++ gitpull.py
@@ -5,7 +5,6 @@ ACCOUNT_NAME = "Admin"
 GIT_USER = "JFaulk1434"
 GIT_REPO = "https://github.com/JFaulk1434/RaspPi.git"
 FPATH = f'/home/{ACCOUNT_NAME}/Documents/pyControl'
-FPATH2 = f'/Users/justinfaulk/Documents/pyControl/RaspPi'
 FNAME = "gitpull.py"
 
 
@@ -14,15 +13,16 @@ def run(*args):
 
 
 def clone():
-    if file_exists(f'{FPATH2}/{FNAME}') == False:
+    if file_exists(f'{FPATH}/{FNAME}') == False:
         subprocess.Popen(['git', 'clone', GIT_REPO])
         print(f'file not located...')
         print(f'pulling from github...')
         print(f'adding {FNAME} to {FPATH}')
     else:
-        subprocess.Popen(['git', 'pull --prune', GIT_REPO])
+        subprocess.Popen(f'git reset --hard')
+        subprocess.Popen(f'git pull')
         print(f'Files already detected...')
-        print(f'ignoring')
+
 # def clone():
 #     subprocess.Popen(['git', 'clone', GIT_REPO])
 #     print("Cloning new files")
